93bb7f478bd5297029c8e0e42a13b4ef286655c3
Iliass JABALI <iliassjabali@gmail.com>
2024-04-12 00:45:16 +0000

    Update import paths in utils.ts, sonner.tsx, page.tsx, posts.tsx, and AddComment.tsx

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,16 +1,15 @@
 import { baseUrl } from "@/lib/utils";
 import { getBlogPosts } from "@/app/yap/utils";
-import { format } from "date-fns";
 
 export default function sitemap() {
   const blogs = getBlogPosts().map((post) => ({
     url: `${baseUrl}/yap/${post.slug}`,
-    lastModified: post.metadata.publishedAt,
+    lastModified: new Date(post.metadata.publishedAt ?? ""),
   }));
 
   const routes = ["", "/yap"].map((route) => ({
     url: `${baseUrl}${route}`,
-    lastModified: format(new Date(), "yyyy-MM-dd"),
+    lastModified: new Date(),
   }));
 
   return [...routes, ...blogs];
